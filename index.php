acedac86b31b0c738c9d6ae1ebf44e7f1fdd899a
Lasse Rafn <lasserafn@gmail.com>
2017-09-24 21:54:41 +0200

    Caching

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ header('Content-type: image/png');
 
 $avatar = new LasseRafn\InitialAvatarGenerator\InitialAvatar();
 
-
 $name = $_GET['name'] ?? 'John Doe';
 $length = (int) ( $_GET['length'] ?? 2 );
 $size = (int) ( $_GET['size'] ?? 64 );
@@ -14,10 +13,15 @@ $fontSize = (double) ($_GET['font-size'] ?? 0.5);
 $background = $_GET['background'] ?? '#000';
 $color = $_GET['color'] ?? '#fff';
 
-
 $initials = (new LasseRafn\Initials\Initials)->generate($name);
 
 // todo get from cache
+$cacheKey = md5("{$initials}-{$length}-{$size}-{$fontSize}-{$background}-{$color}");
+
+if(file_exists(__DIR__ . "/cache/{$cacheKey}.png")) {
+	echo readfile(__DIR__ . "/cache/{$cacheKey}.png");
+	return;
+}
 
 $image = $avatar->name($name)
 		->length($length)
@@ -27,4 +31,7 @@ $image = $avatar->name($name)
 		->color($color)
 		->generate();
 
+$image->save(__DIR__ . "/cache/{$cacheKey}.png");
+
 echo $image->stream('png', 100);
+return;
