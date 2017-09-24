59731cdcb0df9c5fe9f534779fa4e1b12c0a116f
Lasse Rafn <lasserafn@gmail.com>
2017-09-24 21:51:55 +0200

    Variables

--- index.php
+++ index.php
@@ -5,17 +5,26 @@ require_once __DIR__ . '/vendor/autoload.php';
 header('Content-type: image/png');
 
 $avatar = new LasseRafn\InitialAvatarGenerator\InitialAvatar();
-$initials = (new LasseRafn\Initials\Initials)->generate($_GET['name'] ?? 'John Doe');
+
+
+$name = $_GET['name'] ?? 'John Doe';
+$length = (int) ( $_GET['length'] ?? 2 );
+$size = (int) ( $_GET['size'] ?? 64 );
+$fontSize = (double) ($_GET['font-size'] ?? 0.5);
+$background = $_GET['background'] ?? '#000';
+$color = $_GET['color'] ?? '#fff';
+
+
+$initials = (new LasseRafn\Initials\Initials)->generate($name);
 
 // todo get from cache
 
-$image = $avatar->name($_GET['name'] ?? 'John Doe')
-		->length($_GET['length'] ?? 2)
-		->fontSize((double) ($_GET['font-size'] ?? 0.5))
-		->size((int) ($_GET['size'] ?? 64))
-		->background($_GET['background'] ?? '#000')
-		->color($_GET['color'] ?? '#fff')
-		->cache()
+$image = $avatar->name($name)
+		->length($length)
+		->fontSize($fontSize)
+		->size($size)
+		->background($background)
+		->color($color)
 		->generate();
 
 echo $image->stream('png', 100);
