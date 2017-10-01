3aff9791ea0c6035a4836bb4d4ef6d6a5b2c4d1a
Lasse Rafn <lasserafn@gmail.com>
2017-10-01 18:23:12 +0200

    Cache cleanup

--- cron/cleanup.php
+++ cron/cleanup.php
@@ -1,3 +1,13 @@
 <?php
 
-// todo delete avatars older than X days.
+$files            = glob( __DIR__ . '/../cache/*.png' );
+$now              = time();
+$twoDaysInSeconds = 60 * 60 * 24 * 2;
+
+foreach ( $files as $file ) {
+	if ( is_file( $file ) ) {
+		if ( $now - filemtime( $file ) >= $twoDaysInSeconds ) {
+			unlink( $file );
+		}
+	}
+}
