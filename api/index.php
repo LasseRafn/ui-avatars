82a57b7dfccfd497979921be04955da594a19862
Lasse Rafn <lasserafn@gmail.com>
2017-10-13 11:12:16 +0200

    Uppercase + debugging

--- api/index.php
+++ api/index.php
@@ -14,14 +14,16 @@ $avatar = new LasseRafn\InitialAvatarGenerator\InitialAvatar();
 $input  = new \Utils\Input;
 
 if ( ! isset( $_GET['no-cache'] ) && file_exists( __ROOT__ . "/cache/{$input->cacheKey}.png" ) ) {
-	header( 'Expires: ' . gmdate( 'D, d M Y H:i:s \G\M\T', filemtime( __ROOT__ . "/cache/{$input->cacheKey}.png" ) + 172800 ) );
-	header('X-Accel-Redirect: ' .  "/cache/{$input->cacheKey}.png"); // If this part is causing you trouble, remove it and uncomment the two following lines:
-//	$file = fopen( __ROOT__ . "/cache/{$input->cacheKey}.png", 'rb' );
-//	fpassthru( $file );
+	if ( isset( $_GET['debug'] ) ) {
+		$file = fopen( __ROOT__ . "/cache/{$input->cacheKey}.png", 'rb' );
+		fpassthru( $file );
+	} else {
+		header( 'Expires: ' . gmdate( 'D, d M Y H:i:s \G\M\T', filemtime( __ROOT__ . "/cache/{$input->cacheKey}.png" ) + 172800 ) );
+		header( 'X-Accel-Redirect: ' . "/cache/{$input->cacheKey}.png" ); // If this part is causing you trouble, remove it and uncomment the two following lines:
+	}
 
 	exit;
-}
-else {
+} else {
 	header( 'Expires: ' . gmdate( 'D, d M Y H:i:s \G\M\T', time() + 172800 ) );
 }
 
@@ -38,7 +40,10 @@ $image = $avatar->name( $input->name )
 
 $image->save( __ROOT__ . "/cache/{$input->cacheKey}.png", 100 );
 
-header('X-Accel-Redirect: ' .  "/cache/{$input->cacheKey}.png"); // If this part is causing you trouble, remove it and uncomment the following line:
-//echo $image->stream( 'png', 100 );
+if ( isset( $_GET['debug'] ) ) {
+	header( 'X-Accel-Redirect: ' . "/cache/{$input->cacheKey}.png" );
+} else {
+	echo $image->stream( 'png', 100 );
+}
 
 exit;
