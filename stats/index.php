13136372dd899a859d79ff775bf8229bc19cb50c
Lasse Rafn <lasserafn@gmail.com>
2018-03-10 23:27:42 +0100

    Stats

--- stats/index.php
+++ stats/index.php
@@ -6,9 +6,18 @@ require_once __ROOT__ . '/vendor/autoload.php';
 
 header( 'Content-type: application/json' );
 
+$ch = curl_init();
+curl_setopt( $ch, CURLOPT_URL, 'https://dashboard.apricot.dk/api/ui-avatars/5LJwwA7TqnHduCBgi0VmD3U7Yq4oyDZK' );
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+curl_setopt( $ch, CURLOPT_USERAGENT, 'UI Avatars' );
+curl_setopt( $ch, CURLOPT_REFERER, $_SERVER['HTTP_HOST'] );
+
+$response = json_decode( curl_exec( $ch ) );
+curl_close( $ch );
+
 echo json_encode( [
-	'response_time' => round( 2.8671792705, 4 ),
-	'requests'      => number_format( 65000, 0 )
+	'response_time' => round( $response->response_time, 4 ),
+	'requests'      => number_format( $response->requests, 0 )
 ] );
 
 exit;
